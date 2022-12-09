efa5516b5c9054a47f0b86dae3e6194a2e0f5837
Tushar <tusharsrivastava162@gmail.com>
2022-12-09 17:11:19 +0530

    pages with routing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Tushar Srivastava</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
-
+        <ul>
+          <li><Link href='/'>Home</Link></li>
+          <li><Link href='/about'>About me</Link></li>
+          <li><Link href='/about/work'>Work Experience</Link></li>
+        </ul>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>

fa2cc149be3d9be9813e947de651e0c9bcf48b55
Tushar <tusharsrivastava162@gmail.com>
2022-12-10 13:21:21 +0530

    page title and image

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,15 +1,27 @@
 import styles from '../../styles/Home.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function About() {
     return (
     <div className={styles.container}>
-        <h1 className={styles.title}>About Index</h1>
+      <Head>
+        <title>About TS</title>
+      </Head>
+        <h1 className={styles.title}>About Me</h1>
         <ul>
           <li><Link href='/'>Home</Link></li>
           <li><Link href='/about'>About me</Link></li>
           <li><Link href='/about/work'>Work Experience</Link></li>
         </ul>
+        <Image
+          src="/images/profile.jpeg" // Route of the image file
+          height={144} // Desired size with correct aspect ratio
+          width={144} // Desired size with correct aspect ratio
+          alt="TS"
+        />
+        {/* <img src="/images/profile.jpeg" alt="Tushar Srivastava ke Head ka Photo" /> */}
         <p className={styles.description}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolores architecto? Voluptate quis dolorem ex itaque illum harum minus velit! Iusto, et! Quos quia facilis accusantium veniam odio quidem praesentium?
         </p>

6b19a7f8679925cba5b7a09dff46e4c78076bc9f
Tushar <tusharsrivastava162@gmail.com>
2022-12-10 13:55:40 +0530

    prop based conditional rendering

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,21 @@
 import styles from './layout.module.css'
+import Link from 'next/link'
 
-export default function Layout({children}) {
-    return <div id="lout" className={styles.layout_bg_light}>{children}</div>
+export default function Layout({children, home}) {
+    return (
+        <div id="lout" className={styles.layout_bg_light}>
+            <div>
+                {home ? (
+                    <h4>Home Page</h4>
+                ) : (
+                    <h4>
+                        <Link href='/'>Back to home page</Link>
+                    </h4>
+                )}
+            </div>
+            <div>
+                {children}
+            </div>
+        </div>
+    )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <Layout>
+    <Layout home>
       <div className={styles.container}>
         <Head>
           <title>Tushar Srivastava</title>

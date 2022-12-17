266a8caeb262f2aeadcc42d92900334bec7d0b94
Tushar <tusharsrivastava162@gmail.com>
2022-12-17 11:39:49 +0530

    Revert "Revert "navigation component""
    This reverts commit 710adef2a07001d37f604bcd1b89649dec5ef650.

--- pages/about/work.jsx
+++ pages/about/work.jsx
@@ -1,18 +1,14 @@
 import styles from '../../styles/Home.module.css'
-import Link from 'next/link'
 import Layout from '../../components/layout'
+import Navigation from '../../components/navigation'
 
 export default function About(props) {
     return (
       <Layout>
+        <Navigation/>
         <div className={styles.container}>
             <h1 className={styles.title}>Work Experience</h1>
             <h2>SSG {props.last_build_time}</h2>
-            <ul>
-              <li><Link href='/'>Home</Link></li>
-              <li><Link href='/about'>About me</Link></li>
-              <li><Link href='/about/work'>Work Experience</Link></li>
-            </ul>
             <p className={styles.description}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolores architecto? Voluptate quis dolorem ex itaque illum harum minus velit! Iusto, et! Quos quia facilis accusantium veniam odio quidem praesentium?
             </p>
